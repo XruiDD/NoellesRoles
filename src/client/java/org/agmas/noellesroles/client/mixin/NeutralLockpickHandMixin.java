32c7cb33c90327c5fc22f0fb67085afea0ea2121
XruiDD <48876676+XruiDD@users.noreply.github.com>
2025-12-27 16:00:39 +0800

    fix: 隐藏炸弹客的炸弹可见性

--- src/client/java/org/agmas/noellesroles/client/mixin/NeutralLockpickHandMixin.java
+++ src/client/java/org/agmas/noellesroles/client/mixin/NeutralLockpickHandMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.client.render.entity.feature.HeldItemFeatureRenderer;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import org.agmas.noellesroles.ModItems;
 import org.agmas.noellesroles.Noellesroles;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,12 +23,18 @@ public class NeutralLockpickHandMixin {
     @WrapOperation(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getMainHandStack()Lnet/minecraft/item/ItemStack;"))
     private ItemStack hideNeutralLockpickMain(LivingEntity instance, Operation<ItemStack> original) {
         ItemStack ret = original.call(instance);
-        if ((ret.isOf(WatheItems.CROWBAR))&& instance instanceof PlayerEntity player) {
+        if (ret.isOf(WatheItems.CROWBAR) && instance instanceof PlayerEntity player) {
             GameWorldComponent gameWorld = GameWorldComponent.KEY.get(player.getWorld());
             if (gameWorld.isRole(player, Noellesroles.JESTER)
                     || gameWorld.isRole(player, Noellesroles.VULTURE)
                     || gameWorld.isRole(player, Noellesroles.CORRUPT_COP)
                     || gameWorld.isRole(player, Noellesroles.PATHOGEN)) {
+                return ItemStack.EMPTY;
+            }
+        }
+        if(ret.isOf(ModItems.TIMED_BOMB) && instance instanceof PlayerEntity player) {
+            GameWorldComponent gameWorld = GameWorldComponent.KEY.get(player.getWorld());
+            if (gameWorld.isRole(player, Noellesroles.BOMBER)){
                 return ItemStack.EMPTY;
             }
         }
