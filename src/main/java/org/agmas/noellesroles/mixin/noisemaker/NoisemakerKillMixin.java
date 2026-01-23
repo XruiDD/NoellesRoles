9544b2c548e4aa0abfd3a9275b3d0f1f71c98697
XruiDD <48876676+XruiDD@users.noreply.github.com>
2026-01-24 00:31:20 +0800

    feat: 大嗓门现在死亡有全服音效播报和通知

--- src/main/java/org/agmas/noellesroles/mixin/noisemaker/NoisemakerKillMixin.java
+++ src/main/java/org/agmas/noellesroles/mixin/noisemaker/NoisemakerKillMixin.java
@@ -9,7 +9,16 @@ import dev.doctor4t.wathe.game.GameFunctions;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.agmas.noellesroles.ModSounds;
 import org.agmas.noellesroles.Noellesroles;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,9 +30,19 @@ public abstract class NoisemakerKillMixin {
 
     @Inject(method = "killPlayer(Lnet/minecraft/entity/player/PlayerEntity;ZLnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Identifier;Z)V", at = @At(value = "INVOKE", target = "Ldev/doctor4t/wathe/entity/PlayerBodyEntity;setHeadYaw(F)V"))
     private static void noisemakerKill(PlayerEntity victim, boolean spawnBody, PlayerEntity killer, Identifier identifier,boolean force, CallbackInfo ci, @Local PlayerBodyEntity body) {
-        GameWorldComponent gameWorldComponent = (GameWorldComponent) GameWorldComponent.KEY.get(victim.getWorld());
+        GameWorldComponent gameWorldComponent = GameWorldComponent.KEY.get(victim.getWorld());
         if (gameWorldComponent.isRole(victim, Noellesroles.NOISEMAKER)) {
            body.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 20*60, 0));
+           if (victim.getWorld() instanceof ServerWorld serverWorld) {
+               RegistryEntry<SoundEvent> soundEntry = RegistryEntry.of(SoundEvents.ENTITY_ALLAY_DEATH);
+               var seed = serverWorld.random.nextLong();
+               for (ServerPlayerEntity player : serverWorld.getServer().getPlayerManager().getPlayerList()) {
+                   if(gameWorldComponent.isInnocent(player) || player.isSpectator()){
+                       player.networkHandler.sendPacket(new PlaySoundS2CPacket(soundEntry, SoundCategory.PLAYERS, player.getX(), player.getY(), player.getZ(),1.0f, 1.0f,seed));
+                       player.sendMessage(Text.translatable("noellesroles.noisemaker.death_scream"), true);
+                   }
+               }
+           }
         }
     }
 
