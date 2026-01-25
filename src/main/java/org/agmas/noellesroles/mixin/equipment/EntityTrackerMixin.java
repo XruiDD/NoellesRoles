78ed99733402be956210c0f1a878d3fef0839f22
XruiDD <48876676+XruiDD@users.noreply.github.com>
2026-01-25 14:53:04 +0800

    fix: 修复mixin问题

--- src/main/java/org/agmas/noellesroles/mixin/equipment/EntityTrackerMixin.java
+++ src/main/java/org/agmas/noellesroles/mixin/equipment/EntityTrackerMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.EntityEquipmentUpdateS2CPacket;
 import net.minecraft.server.network.PlayerAssociatedNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerChunkLoadingManager;
 import org.agmas.noellesroles.util.HiddenEquipmentHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,10 +18,8 @@ import java.util.Set;
 
 /**
  * 服务端装备广播隐藏 Mixin
- * <p>
- * 拦截装备数据包广播，为每个接收者单独处理隐藏逻辑。
  */
-@Mixin(ServerChunkLoadingManager.EntityTracker.class)
+@Mixin(targets = "net.minecraft.server.world.ServerChunkLoadingManager$EntityTracker")
 public class EntityTrackerMixin {
 
     @Shadow
