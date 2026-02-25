d802cdf036ce6ee92fd1521131c5dac5e13a332a
XruiDD <48876676+XruiDD@users.noreply.github.com>
2026-02-25 21:54:36 +0800

    fix: 修复mixin

--- src/main/java/org/agmas/noellesroles/mixin/scavenger/ScavengerBodyCanHitMixin.java
+++ src/main/java/org/agmas/noellesroles/mixin/scavenger/ScavengerBodyCanHitMixin.java
@@ -3,6 +3,7 @@ package org.agmas.noellesroles.mixin.scavenger;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import dev.doctor4t.wathe.entity.PlayerBodyEntity;
+import net.minecraft.entity.LivingEntity;
 import org.agmas.noellesroles.scavenger.HiddenBodiesWorldComponent;
 import org.spongepowered.asm.mixin.Mixin;
 
@@ -12,15 +13,16 @@ import org.spongepowered.asm.mixin.Mixin;
  * 当尸体被清道夫隐藏时返回false，使准星射线穿透隐藏尸体，
  * 玩家可以正常操作后面的方块。
  */
-@Mixin(PlayerBodyEntity.class)
+@Mixin(LivingEntity.class)
 public class ScavengerBodyCanHitMixin {
 
     @WrapMethod(method = "canHit")
     private boolean noellesroles$disableScavengerBodyTargeting(Operation<Boolean> original) {
-        PlayerBodyEntity self = (PlayerBodyEntity) (Object) this;
-        HiddenBodiesWorldComponent hiddenBodies = HiddenBodiesWorldComponent.KEY.get(self.getWorld());
-        if (hiddenBodies.isHidden(self.getPlayerUuid())) {
-            return false;
+        if((Object)this instanceof PlayerBodyEntity self){
+            HiddenBodiesWorldComponent hiddenBodies = HiddenBodiesWorldComponent.KEY.get(self.getWorld());
+            if (hiddenBodies.isHidden(self.getPlayerUuid())) {
+                return false;
+            }
         }
         return original.call();
     }
